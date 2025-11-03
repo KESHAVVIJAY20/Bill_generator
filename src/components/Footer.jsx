af9bff5dd236efe3ada6fc8d4704ecc241d8c4a1
KESHAVVIJAY20 <keshavvijay8@gmail.com>
2025-11-03 12:39:37 +0530

    added a seal stamp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
 import React from "react";
-
+import SealStamp from '../assets/v_stamp.png';
 export default function Footer() {
   return (
     <div className="mt-10">
-    <div className="flex justify-end items-center me-28">M/S Vijay Jewellers</div>
+    {/* <div className="flex justify-end items-center me-28 absolute"><p className="relative">M/S Vijay Jewellers</p>
+    <img src={SealStamp} className="relative" alt="" /></div> */}
+    <div className="absolute right-28 flex flex-col items-center">
+  <div className="relative">
+    <p className="text-sm font-semibold">M/S Vijay Jewellers</p>
+    <img
+      src={SealStamp}
+      alt="Seal Stamp"
+      className="absolute top-[-70px] left-1/2 -translate-x-1/2 rotate-[-15deg]  h-auto min-w-48 opacity-100 pointer-events-none"
+    />
+  </div>
+</div>
+
+
+
       <div className="flex justify-between items-end mt-16">
         <ul className="space-y-5 text-xl">
           <li>सोने की शुद्धता की गारंटी</li>
